61b9027f331f4320eb1b569124c4f11fa95848f5
Tesseract22 <tesseractcat@proton.me>
2025-08-11 09:57:04 +0800

    add vue lsp

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -69,5 +69,46 @@ require'lspconfig'.zls.setup{
     -- log_level = vim.lsp.protocol.MessageType.Log,
     -- message_level = vim.lsp.protocol.MessageType.Log,
 }
+lspconfig.pylsp.setup{
+
+}
 vim.g.zig_fmt_autosave = 0
 
+local vue_language_server_path = vim.fn.expand("$MASON/packages/vue-language-server/node_modules/@vue/language-server")
+local tsserver_filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' }
+local vue_plugin = {
+  name = '@vue/typescript-plugin',
+  location = vue_language_server_path,
+  languages = { 'vue' },
+  configNamespace = 'typescript',
+}
+local vtsls_config = {
+  settings = {
+    vtsls = {
+      tsserver = {
+        globalPlugins = {
+          vue_plugin,
+        },
+      },
+    },
+  },
+  filetypes = tsserver_filetypes,
+}
+
+local ts_ls_config = {
+  init_options = {
+    plugins = {
+      vue_plugin,
+    },
+  },
+  filetypes = tsserver_filetypes,
+}
+
+-- If you are on most recent `nvim-lspconfig`
+local vue_ls_config = {}
+
+-- If using vtsls
+lspconfig.vtsls.setup(vtsls_config)
+-- If using ts_ls
+lspconfig.ts_ls.setup(ts_ls_config)
+lspconfig.volar.setup(vue_ls_config)
